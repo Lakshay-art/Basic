3c8e04474e3da049ca9fba05fe5af8dcf5169441
Lakshay Gupta <lakshay0173.cse19@chitkara.edu.in>
2021-10-15 02:50:28 +0530

    how to use animation with useEffect

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App (){
             <div className="App">
                
        <Sendingprops/> 
-       <UseRefuseEffect/>
+       <UseRefuseEffect class="useName"/>
     </div>
           
     );
--- src/components/UseRefuseEffect.js
+++ src/components/UseRefuseEffect.js
@@ -1,9 +1,11 @@
 import React, { useEffect, useRef, useState } from 'react'
 import {Jumbotron,Button,FormControl,Form,Row,Col} from 'react-bootstrap'
-import Propsreciever from './Propsreciever';
+import Propsreciever from './Propsreciever'
+import $ from 'jquery'
+
 export default function UseRefuseEffect() {
     
-    const [state, setstate] = useState()
+    const [state, setstate] = useState("^o^")
     
      const cityRef=useRef();//declaring an useRef;
 
@@ -15,25 +17,46 @@ export default function UseRefuseEffect() {
         console.log(state);
     },[])
 
+    useEffect(()=>{
+        document.getElementById("ff").innerHTML= `<h5 style={{padding:"1px",margin:"0px"}}>${state}</h5>`
+    })
+
     const submitt=(e)=>{
         e.preventDefault();
-        setstate(cityRef.current.value)
+        if(cityRef.current.value!="")
+        setstate(cityRef.current.value.slice(0,2).toUpperCase())
+        HideName();
     }
-
+    const HideName = ()=>{
+      //  $('#useName').animate({left:"20%"}, 100);
+        $('#useName').animate({left:"103%"}, 800);
+      //  $('#useName').focus()
+    }
+    const ShowName = ()=>{
+       // $('#useName').animate({left:"33%"}, 800);
+        $('#useName').animate({left:"27%"}, 800);
+    }
+    useEffect(()=>{
+        
+        ShowName()
+    },[])
     return (
         <>
-        <Jumbotron id="searchj" className="shadow" style={{ width:"30%", padding: "4px", margin: "auto",marginBottom:"20px", borderRadius: "30px", position: "absolute",top:"50%",left:"32%",height:"100px" ,zIndex: "101",minWidth:"150px" }}>
+        <Jumbotron id="useName" className="shadow" style={{ width:"30%", padding: "4px", margin: "auto",marginBottom:"20px", borderRadius: "30px", position: "fixed",top:"50%",left:"-40%",height:"100px" ,zIndex: "101",minWidth:"150px" }}>
         <Form inline className=' mx-auto' style={{ width:"70%"}} onSubmit={submitt} >
             <FormControl id="searchf" type='text' placeholder='..' href="#" className='w-100 ' style={{ borderRadius: "25px" }} ref={cityRef} />
             <Button type='submit' className='invisible w-100'></Button>
             <Row style={{margin:"auto"}}><Col style={{padding:"2px"}}> <h4 style={{textAlign:"center",width:"40%",padding:"0px"}}>NAME</h4></Col>
-            <Col  style={{padding:"2px"}}><Button type='submit' style={{fontSize:"10px",padding:"4px"}}>;)</Button></Col>
+            <Col  style={{padding:"2px"}}><Button type='submit'  style={{fontSize:"10px",padding:"4px"}}>;)</Button></Col>
             </Row>
            
             
         </Form>
     </Jumbotron>
-    <h1>{state}</h1>
+    <button id="ff" type="button" class="btn btn-secondary btn-sm rounded-pill ml-auto" onClick={ShowName}>
+        
+          </button>
+    
     </>
     )
 }
